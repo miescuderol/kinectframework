8b4564bf0977689452825357603209c7c765f955
pabloarubi@gmail.com <pabloarubi@gmail.com@2f34404a-e6de-cb6f-0bd9-abac5a06feb8>
2012-11-10 22:05:06 +0000

    Se agregaron comentarios de Doxygen

--- trunk/KinectFramework/KinectFramework/Aplicacion.h
+++ trunk/KinectFramework/KinectFramework/Aplicacion.h
@@ -5,15 +5,37 @@
 #include "Kinect.h"
 #include <vector>
 
+/// Clase básica para construir cualquier juego.
+/**
+* La clase aplicación maneja el game-loop. Conoce los subsistemas del juego, y los niveles.
+* El orden de inicialización/actualización de los Subsistemas es configurable por el programador.
+*/
 class Aplicacion
 {
 public:
 	Aplicacion(void);
 	~Aplicacion(void);
-	void setGrafoJuego(Grafo * grafo);
-	void run();
-	void addSubsistemaPreNivel(Subsistema * subsistema);
+
+	void setGrafoJuego(Grafo * grafo); /**< Agrega el grafo de niveles. */
+
+	void run(); 
+	/**< 
+	 * Inicia el game-loop. 
+	 * Primero llama al setup de todos los subsistemas, carga el primer nivel,
+	 * después inicia el bucle update-draw.
+	 */
+
+	void addSubsistemaPreNivel(Subsistema * subsistema); 
+	/**< 
+	 * Agrega un subsistema que va a ser inicializado/actualizado
+	 * antes de la inicializacion/actualización del nivel activo. 
+	 */
+
 	void addSubsistemaPostNivel(Subsistema * subsistema);
+	/**< 
+	 * Agrega un subsistema que va a ser inicializado/actualizado
+	 * después de la inicializacion/actualización del nivel activo. 
+	 */
 
 private:
 
